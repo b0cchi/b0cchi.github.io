7f3db736b16dea72d58a34c0bc049c5f04df8eee
b0cchi <b0cchi@outlook.jp>
2025-06-04 12:44:49 +0000

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
+'use client';
 import Header from './layout/Header';
-
-export const metadata = {
-	title: 'My Portfolio - Home',
-	description: 'Welcome to my portfolio!',
-};
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<main className="pt-20 px-4 max-w-4xl mx-auto">
+			<main className="relative pt-24 px-6 left-1/2 -translate-x-1/2 w-[90%] max-w-5xl">
 				<h1 className="text-3xl font-bold mb-4">Hi,</h1>
-				<p className="mb-32">I&apos;m b0cchi</p>
-				<p className="mb-8">日本人ですぞ</p>
+				<p className="mb-2">I&apos;m b0cchi</p>
+				<div className="text-2xl sm:text-4xl font-semibold text-center text-silate-900 mb-8">
+					<Typewriter
+						options={{
+							strings: [
+								'Full Stack Developer',
+								'Always Learning',
+								'Code. Run. Repeat :)',
+							],
+							autoStart: true,
+							loop: true,
+						}}
+					/>
+				</div>
 				<div className="h-[2000px] bg-gray-100">{/* スクロール領域 */}</div>
 			</main>
 		</>
